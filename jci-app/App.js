3d784a24879586f563b14f5739a193cffe23bb5f
David Leghorn <davidleghorn@wipro.com>
2017-11-02 11:07:17 +0000

    some folder structure added

--- jci-app/App.js
+++ jci-app/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigatorIOSApp} from 'views/ViewNavigator';
 
 export default class App extends React.Component {
   render() {
     return (
+      <NavigatorIOSApp></NavigatorIOSApp>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
